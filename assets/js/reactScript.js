09be5646ce508c63a71c84cf27a4ce3645fcd8e5
akhilkuttipurath <akhilkuttipurath@qburst.com>
2015-03-24 18:33:45 +0530

    deleting option is added , initial values getting from json

--- assets/js/reactScript.js
+++ assets/js/reactScript.js
@@ -1,80 +1,89 @@
 
-
 /** @jsx React.DOM */
 
-
 $(document).ready(function() {
-
-var List = React.createClass({
-  render: function(){
-    return (
-      <ul id="add-list" onClick = {this.addId}>
-      {
-        this.props.items.map(function(item) {
-          return <li  key={item}> {item} </li>
-
-        })
-       }
-      </ul>
-    )
-  },
-  addId : function(){
-    return (
-        $('li').each(function (i) {
-         
-            $(this).attr('id', 'task' + i);
-            i++;
-
-             alert(this.id);
-        })
+  var List = React.createClass({
+    render: function(){
+      return (
+        <ul id="add-list" onClick = {this.removeItem}>
+        {
+          this.props.items.map(function(item) {
+            return <li key={item}> {item} </li>
+          })
+         }
+        </ul>
       )
-  },
-  removeItem : function(){
-  	$("#add-list li").click(function(){
-  		   alert($(this).attr('id'));
-  	})
-  }
-});
+    },
+    // addId : function(){
+    //   return (
+    //       $('li').each(function (i) {     
+    //           $(this).attr('id', 'task' + i);
+    //           i++;
+    //       })s
+         
+    //     )
+    // },
+    removeItem : function(){
+    	// $("#add-list to_complete").click(function(){
+     //     alert('hi')
+    	// })
+    $('#add-list li').click( function () {
+        $(this).remove();
+    });
 
-// var CompletedTask = React.createClass({
-// 	addCompleted: function(){
+    }
+  });
 
-// 	}
-// })
-var FilteredList = React.createClass({
-  filterList: function(){    
-		    updatedList = this.state.initialItems;	
-	      	newItem = $('#newToDo').val();
-	      	updatedList.push(newItem);
-	      	$('#newToDo').val('');	
-	    	this.setState({items: updatedList});
-	    	this.setState({initialItems: updatedList});	    	
-  },
-  getInitialState: function(){
-     return {
-       initialItems: [
-         "Morning walk",
-         "Make vanilla pudding",
-         "Gym"
-       ],
-       items: []
-     }
-  },
-  componentWillMount: function(){
-    this.setState({items: this.state.initialItems})
-  },
-  render: function(){
-    return (
-      <div className="filter-list">
-	      <div class="btn-wrapper">
-	       <button id="addBtn" onClick={this.filterList}> Add </button>
-	      <input type="text" id="newToDo" placeholder="To Do" />
-	     </div>
-      <List items={this.state.items}/>
-      </div>
-    );
-  }
-});
+  // var CompletedTask = React.createClass({
+  // 	addCompleted: function(){
 
-React.renderComponent(new FilteredList(), document.getElementById('container'));
+  // 	}
+  // })
+  var FilteredList = React.createClass({
+    filterList: function(){    
+  		    updatedList = this.state.initialItems;	
+  	      	newItem = $('#newToDo').val();
+  	      	updatedList.push(newItem);
+  	      	$('#newToDo').val('');	
+  	    	this.setState({items: updatedList});
+  	    	this.setState({initialItems: updatedList});	    	
+    },
+    getInitialState: function(){
+       return {
+         items: []
+       }
+    },
+    componentWillMount: function(){
+      $.ajax({
+        url: "assets/json/toDo.json",
+        dataType: 'json',
+        success: function(data) {
+          var initialItem = data.initial_task
+          var task_collector = [];
+          for(i=0;i<3;i++){
+            var task;
+            task = ("init_task"+i);
+            task_collector.push(initialItem[0][task]);
+          }
+          this.setState({initialItems: task_collector}); 
+          this.setState({items: task_collector});       
+        }.bind(this)
+      });
+    },
+    // componentDidUpdate: function(prevProps, prevState) {
+    // localStorage.state = JSON.stringify(this.state);
+    // },
+    render: function(){
+      return (
+        <div className="filter-list">
+  	      <div class="btn-wrapper">
+  	       <button id="addBtn" onClick={this.filterList}> Add </button>
+  	      <input type="text" id="newToDo" placeholder="To Do" />
+  	   </div>
+        <List items={this.state.items}/>
+        </div>
+      );
+    }
+  });
+  React.renderComponent(<FilteredList/>, document.getElementById('container'));
 }); 
